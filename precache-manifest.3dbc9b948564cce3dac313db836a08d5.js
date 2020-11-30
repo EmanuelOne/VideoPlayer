8fe08760582eda7dd7c364fd36330c07492607e0
EmanuelOne <emanuel4mechie@gmail.com>
2020-11-30 23:38:05 +0100

    Updates

---
+++ precache-manifest.3dbc9b948564cce3dac313db836a08d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e24d0e72c2462c630651e09c825b1a",
+    "url": "/VideoPlayer/index.html"
+  },
+  {
+    "revision": "eba026efedfffa8ea373",
+    "url": "/VideoPlayer/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "b51a263960452f1338a9",
+    "url": "/VideoPlayer/static/css/main.c6969a70.chunk.css"
+  },
+  {
+    "revision": "eba026efedfffa8ea373",
+    "url": "/VideoPlayer/static/js/2.f30d1ba0.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/VideoPlayer/static/js/2.f30d1ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51a263960452f1338a9",
+    "url": "/VideoPlayer/static/js/main.5fe19495.chunk.js"
+  },
+  {
+    "revision": "abc33e804e20fb14b462",
+    "url": "/VideoPlayer/static/js/runtime-main.9f84aa7e.js"
+  }
+]);
