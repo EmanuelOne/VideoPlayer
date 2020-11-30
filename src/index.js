314bfd6f627c87e7abb2c4884378da7feea39a0a
Emmanuel Ifeanyi MECHIE <64092981+EmanuelOne@users.noreply.github.com>
2020-11-30 23:40:23 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./WeatherApp/App.jsx";
+// import App from "./React_Router/App";
+// import App from "./MusicApp/Play";
+// import App from "./QuizApp/App";
+import App from "./Movie/App";
+ReactDOM.render(<App />, document.getElementById("root"));
